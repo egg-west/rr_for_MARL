dc4da90d0a44b0497264d9771c2efdc10e60adb5
LinjX <lin.svc@outlook.com>
2025-06-09 22:38:44 +0800

    set noise

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -7,7 +7,7 @@ from .rnn_feature_agent import RNNFeatureAgent
 from .rnn_sd_agent import RNN_SD_Agent
 
 REGISTRY["rnn"] = RNNAgent
-#REGISTRY["task_encoder_rnn"] = TaskEncoderRNNAgent
+REGISTRY["task_encoder_rnn"] = TaskEncoderRNNAgent
 #REGISTRY["availableA_prediction_rnn"] = AuxiliaryTaskRNNAgent
 #REGISTRY["availableA_contrastive_rnn"] = ContrastiveRNNAgent
 #REGISTRY["rnn_ns"] = RNNNSAgent
